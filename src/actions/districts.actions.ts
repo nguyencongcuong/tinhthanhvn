33a15427eacfccefcba0d11429aa2d6272ff1a8b
Cuong Nguyen <c.ng.devie@gmail.com>
2026-08-03 17:21:55 +0700

    feat: add districts.pre.byprovincecode lookup

--- src/actions/districts.actions.ts
+++ src/actions/districts.actions.ts
@@ -1,8 +1,9 @@
-import { PRE_MERGER_DISTRICTS, PRE_MERGER_DISTRICTS_BY_CODE, PRE_MERGER_DISTRICTS_BY_ID, PRE_MERGER_DISTRICTS_BY_NAME } from "../data/pre-merger-districts";
+import { PRE_MERGER_DISTRICTS, PRE_MERGER_DISTRICTS_BY_CODE, PRE_MERGER_DISTRICTS_BY_ID, PRE_MERGER_DISTRICTS_BY_NAME, PRE_MERGER_DISTRICTS_BY_PROVINCE_CODE } from "../data/pre-merger-districts";
 
 export const districts = {    
     pre: {
         all: () => PRE_MERGER_DISTRICTS,
+        byProvinceCode: (provinceCode: string) => PRE_MERGER_DISTRICTS_BY_PROVINCE_CODE[provinceCode] || [],
         byCode: (code: string) => PRE_MERGER_DISTRICTS_BY_CODE.get(code),
         byId: (id: number) => PRE_MERGER_DISTRICTS_BY_ID.get(id),
         byName: (name: string) => PRE_MERGER_DISTRICTS_BY_NAME.get(name),
